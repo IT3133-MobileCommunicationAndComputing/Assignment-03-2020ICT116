8f1030b154039a2e178120777792d1ad9c3f4d7d
Chesintha <139671816+Chesintha@users.noreply.github.com>
2025-01-05 12:57:35 +0000

    App.js changed

--- UovStudentCare/App.js
+++ UovStudentCare/App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import { PaperProvider } from "react-native-paper";
+
+const AppNavigator = createNativeStackNavigator();
+
+const applicationTitle = "UOV Student Care";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <AppNavigator.Navigator
+            screenOptions={{
+              headerStyle: { backgroundColor: "#70116d" },
+              headerTitleStyle: { color: "#fff" },
+              headerTitleAlign: "center",
+              headerTintColor: "#fff",
+            }}
+            initialRouteName="Login"
+          >
+            <AppNavigator.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{ title: applicationTitle }}
+            />
+            <AppNavigator.Screen
+              name="Profile"
+              component={ProfileScreen}
+              options={{ title: applicationTitle }}
+            />
+          </AppNavigator.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
